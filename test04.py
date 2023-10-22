d67988cf4f4ce1e905766702be04d6dcc01cd33d
Anthony Ibrail Ortega Méndez <anthony.ibrail@gmail.com>
2023-10-22 15:32:40 -0600

    tests

---
+++ test04.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+# Definimos los factores y sus niveles
+factores = ["A", "B", "C", "D", "E", "F", "G"]
+niveles = ["-1", "1"]
+
+# Generamos las combinaciones de tratamientos
+combinaciones = np.array(np.meshgrid(*[niveles for _ in range(len(factores))], indexing="ij")).T.reshape(-1, len(factores))
+
+# Definimos las variables de respuesta
+variables_respuesta = np.random.uniform(0, 1.5, size=combinaciones.shape[0])
+
+# Reducción de la variable de respuesta a solo 4 decimales
+variables_respuesta = np.around(variables_respuesta, decimals=4)
+
+# Suma de una distribución normal a la variable de respuesta
+variables_respuesta += np.random.normal(0.1, 0.1, size=variables_respuesta.shape[0])
+
+# Generamos los datos del experimento
+datos = np.column_stack((combinaciones, variables_respuesta))
+
+# Guardamos los datos en un archivo de Excel
+df = pd.DataFrame(datos, columns=factores + ["Respuesta"])
+df.to_excel("datos02.xlsx")
